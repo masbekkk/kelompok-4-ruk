d4360759c8e2e2b7e683cb732687c85e6ce7200c
Muhammad faris shafwan <73006228+Faris072@users.noreply.github.com>
2024-10-03 17:39:07 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -118,7 +118,7 @@ public class Main {
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Input tidak valid. Masukkan angka untuk pilihan menu.");
-                scanner.nextLine(); 
+                scanner.nextLine(); // bisa langsung di kasih \n pada baris atasnya. -> kiss
             }
         }
     }
